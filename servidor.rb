16b04911b22f4e4c95c6390a466fb3db64c5afbf
Lucas Affonso Xavier de Morais <lucas_axm@hotmail.com>
2015-05-19 20:45:57 -0300

    template de servidor adicionado.

---
+++ servidor.rb
@@ -0,0 +1,11 @@
+require 'socket'               # Get sockets from stdlib
+port = ARGV[0]
+
+server = TCPServer.open(port)
+loop {
+  client = server.accept
+  puts "Mensagem recebida: #{client.recv(100)}"
+  client.send(Time.now.ctime,0)
+  client.send "Ending connection. Bye!",0
+  client.close
+}
