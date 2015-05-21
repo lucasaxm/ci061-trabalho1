777ce2392d19bff2790e70b59a005c0f44790c54
Lucas Affonso Xavier de Morais <lucas_axm@hotmail.com>
2015-05-21 20:35:37 -0300

    Logica do "OK" e "NOK" implementada no cliente.

--- cliente.rb
+++ cliente.rb
@@ -1,13 +1,51 @@
+#!/usr/bin/env ruby1.9.1
+
 require 'socket'
 
 3.times do |i|
 	puts "Digite o nome do servidor #{i}: "
-	hostnames[i]=gets.chomp
+	hostnames[i]=gets.chomp	# array com nome dos servidores
 	puts "Digite a porta do servidor #{i}: "
-	portas[i]=gets.chomp
-	socket[hostnames[i]] = TCPSocket.open(hostnames[i], portas[i])
+	portas[i]=gets.chomp	# array com porta dos servidores
+	socket[hostnames[i]] = TCPSocket.open(hostnames[i], portas[i])	# hash com chave="nome do host" e valor=TCPSocket
 end
-puts "Digite blablabla" # parei aqui
+opcao = 0
+while opcao!=2
+	print "
+	Escolha uma opção
+	1 - Editar arquivo no servidor.
+	2 - Sair.
+	? "
+	opcao = gets.chomp.to_i
+
+	if opcao==1
+		okArray=[]
+	 	hostnames.each do |host|
+	 		puts "Enviando requisição para #{host}"
+	 		socket[host].send("EDIT",0)
+	 		resposta = socket[host].recv(100)
+	 		if resposta == "OK"
+	 		{
+	 			okArray<<host
+	 			puts "#{host} respondeu OK."
+	 		}
+	 		elsif resposta=="NOK" 
+	 		{
+	 			puts "#{host} esta ocupado."
+	 			okArray.each { |okHost|
+	 				socket[host].send("ABORT",0)
+	 				puts "ABORT enviado para #{okHost}."
+	 			}
+	 			break
+	 		}
+	 	end
+	 	if okArray.size == hostnames.size
+	 		# enviar COMMIT com a alteração pra todo mundo.
+	 	end
+	 	
+	end
+end
+
 s.send(texto,0)
 puts s.recv(100)
 
