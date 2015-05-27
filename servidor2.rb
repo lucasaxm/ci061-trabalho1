83a3524843961f2d7cdbbb7ff28c0f1c27df9e37
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2015-05-27 20:24:58 -0300

    servidor feito, falta alterar o dado
    Signed-off-by: Evelim Carla Ribeiro <ecr10@inf.ufpr.br>

--- servidor2.rb
+++ servidor2.rb
@@ -1,14 +1,32 @@
 #!/usr/bin/env ruby1.9.1
 require 'socket'               # Get sockets from stdlib
+require 'thread'
 port = ARGV[0]
+
+mutex = Mutex.new
 
 server = TCPServer.open(port)
 loop {
 #servidor com mais de um cliente
 	Thread.start(server.accept) do |client|
-	  puts "Mensagem recebida: #{client.recv(100)}"
-	  client.send(Time.now.ctime,0)
-	  client.send "Ending connection. Bye!",0
-	  client.close                # Disconnect from the client
+		  puts "Recebendo uma requisicao: #{client.recv(100)}"
+		  requisicao = client.recv(100)
+		  if (requisicao == "EDIT") && mutex.locked?
+		  	client.send("NOK",0)
+		  elsif (requisicao == "EDIT") && !mutex.locked?
+			client.send("OK",0)
+			requisicao = client.recv(100)
+			if requisicao == "COMMIT"
+			  mutex.synchronize do
+				  #faz a mudança do dado
+				  # Disconnect from the client
+
+			  end	
+			elsif requisicao == "ABORT"
+				puts "Operacao Cancelada"
+			end						  			
+		  client.send ("Fechando Conexão. Até mais!",0)
+		  client.close          
+		  end
 	end
 }  
