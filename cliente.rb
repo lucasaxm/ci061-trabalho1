c6eae4c3776e94da2f50409212edc92d47edb915
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2015-06-01 19:47:37 -0300

    transformada variaveis locais(file, keyword) em variaveis globais.
    Signed-off-by: Evelim Carla Ribeiro <ecr10@inf.ufpr.br>

--- cliente.rb
+++ cliente.rb
@@ -7,17 +7,17 @@ require "awesome_print"
 #        metodos       #
 #======================#
 
-def log(file, msg, val)
+def log(msg, val)
 	case val
 		when 0
-			file.puts msg
+			$file.puts msg
 			puts msg
 		when 1
-			file.puts msg
+			$file.puts msg
 		when 2
 			puts msg
 		when 3
-			file.print msg
+			$file.print msg
 			print msg
 		when 4
 			print msg
@@ -26,12 +26,12 @@ def log(file, msg, val)
 	end
 end
 
-def cabecalho(file)
-	log(file, " -----------------------------------------------------------------------",1)
-	log(file, "| Prof. Elias P. Duarte Jr.  -  Disciplina Redes 2                      |",1)
-	log(file, "| Trabalho que implementa a Consistência de Dados com 2PC Simplificado  |",1)
-	log(file, " -----------------------------------------------------------------------",1)
-	log(file, "Cliente \n",1)
+def cabecalho
+	log(" -----------------------------------------------------------------------",1)
+	log("| Prof. Elias P. Duarte Jr.  -  Disciplina Redes 2                      |",1)
+	log("| Trabalho que implementa a Consistência de Dados com 2PC Simplificado  |",1)
+	log(" -----------------------------------------------------------------------",1)
+	log("Cliente \n",1)
 end
 
 numServers=2
@@ -39,27 +39,27 @@ hostnames = []
 portas = []
 socket = []
 filename = "cliente.txt"
-file = File.new(filename, "w+")
-cabecalho(file)
+$file = File.new(filename, "w+")
+cabecalho
 numServers.times do |i|
-	log(file, "Digite o nome do servidor #{i}: ",2)
+	log("Digite o nome do servidor #{i}: ",2)
 	hostnames[i]=gets.chomp	# array com nome dos servidores
-	log(file, "Digite a porta do servidor #{i}: ",2)
+	log("Digite a porta do servidor #{i}: ",2)
 	portas[i]=gets.chomp	# array com porta dos servidores
-	log(file, "Inserindo o servidor #{hostnames[i]} com #{portas[i]} para poder fazer conexão.",1)
+	log("Inserindo o servidor #{hostnames[i]} com #{portas[i]} para poder fazer conexão.",1)
 end
 opcao = 0
 system "clear"
 while opcao!=4
-	log(file,"Este cliente esta conectado aos seguintes servidores: ",0)
+	log("Este cliente esta conectado aos seguintes servidores: ",0)
 	numServers.times do |i|
 		if i<numServers-1
-			log(file, "#{hostnames[i]}:#{portas[i]}, ",3)
+			log("#{hostnames[i]}:#{portas[i]}, ",3)
 		else
-			log(file, "#{hostnames[i]}:#{portas[i]}.",0)
+			log("#{hostnames[i]}:#{portas[i]}.",0)
 		end
 	end
-	log(file, "
+	log("
 	Escolha uma opção
 	1 - Trocar palavra-chave.
 	2 - Ver palavra-chave.
@@ -68,7 +68,7 @@ while opcao!=4
 	? ", 3)
 
 	opcao = gets.chomp.to_i
-	log(file,"opcao digitada #{opcao}",1)
+	log("opcao digitada #{opcao}",1)
 
 	case opcao
 		when 1 # SETKEY
@@ -79,45 +79,45 @@ while opcao!=4
 			end
 	
 			numServers.times do |i|
-				log(file, "",1)
-				log(file, "Enviando requisição para #{hostnames[i]}:#{portas[i]}...",0)
+				log("",1)
+				log("Enviando requisição para #{hostnames[i]}:#{portas[i]}...",0)
 				socket[i].send("SETKEY",0)
-				log(file, "Requisição SETKEY enviada. Aguardando resposta...",0)
+				log("Requisição SETKEY enviada. Aguardando resposta...",0)
 				resposta = socket[i].recv(100)
 				if resposta == "OK"
 					okArray << i
-					log(file,"#{hostnames[i]}:#{portas[i]} respondeu OK.",0)
+					log("#{hostnames[i]}:#{portas[i]} respondeu OK.",0)
 				elsif resposta=="NOK"
-					log(file, "#{hostnames[i]}:#{portas[i]} está ocupado.",0)
+					log("#{hostnames[i]}:#{portas[i]} está ocupado.",0)
 				else
-					log(file,"Resposta recebida: '#{resposta}'.",0)
-					log(file,"Resposta inválida.",0)
+					log("Resposta recebida: '#{resposta}'.",0)
+					log("Resposta inválida.",0)
 				end
 			end
 			
-			log(file, "",1)	
+			log("",1)	
 			if okArray.size<numServers
 				okArray.each do |i|
-					log(file, "Enviando ABORT para #{hostnames[i]}:#{portas[i]}...",0)
+					log("Enviando ABORT para #{hostnames[i]}:#{portas[i]}...",0)
 					socket[i].send("ABORT",0)
-					log(file, "ABORT enviado para #{hostnames[i]}:#{portas[i]}....",0)
+					log("ABORT enviado para #{hostnames[i]}:#{portas[i]}....",0)
 				end
 			else
 				# enviar COMMIT com a alteração pra todo mundo.
-				log(file, "Digite a nova palavra-chave:",0)
+				log("Digite a nova palavra-chave:",0)
 				keyword = gets.chomp
-				log(file, "A palavra chave digita é: #{keyword}",0)
+				log("A palavra chave digita é: #{keyword}",0)
 				numServers.times do |i|
-	 				log(file, "enviando um COMMIT para o #{socket[i]}",1)
+	 				log("enviando um COMMIT para o #{socket[i]}",1)
 					socket[i].send("COMMIT",0)
 					confirmacao = socket[i].recv(100)
-					log(file, "recebendo a confirmação #{confirmacao} do #{socket[i]}",1)
+					log("recebendo a confirmação #{confirmacao} do #{socket[i]}",1)
 					if confirmacao=="ACK"
 						socket[i].send(keyword, 0)
-						log(file, "Palavra-chave '#{keyword}' enviada para o host #{hostnames[i]}:#{portas[i].to_i} com sucesso!",0)
+						log("Palavra-chave '#{keyword}' enviada para o host #{hostnames[i]}:#{portas[i].to_i} com sucesso!",0)
 					else
-						log(file, "Falha ao receber confirmação do servidor.",0)
-						log(file, "Mensagem do servidor: '#{confirmacao}'.",0)
+						log("Falha ao receber confirmação do servidor.",0)
+						log("Mensagem do servidor: '#{confirmacao}'.",0)
 					end					
 				end
 			end
@@ -133,51 +133,51 @@ while opcao!=4
 			end
 	
 			numServers.times do |i| # envia GETFILE
-				log(file, "",1)
-				log(file,"Enviando requisição para #{hostnames[i]}:#{portas[i]}...",0)
+				log("",1)
+				log("Enviando requisição para #{hostnames[i]}:#{portas[i]}...",0)
 				socket[i].send("GETKEY",0)
-				log(file,"Requisição GETKEY enviada. Aguardando resposta...",0)
+				log("Requisição GETKEY enviada. Aguardando resposta...",0)
 				resposta = socket[i].recv(100)
 				if resposta == "OK"
 					okArray << i
-					log(file, "#{hostnames[i]}:#{portas[i]} respondeu OK.",0)
+					log("#{hostnames[i]}:#{portas[i]} respondeu OK.",0)
 				elsif resposta=="NOK" 
-					log(file, "#{hostnames[i]}:#{portas[i]} está ocupado.",0)
+					log("#{hostnames[i]}:#{portas[i]} está ocupado.",0)
 				else
-					log(file, "Resposta recebida: '#{resposta}'.",0)
-					log(file, "Resposta inválida.",0)
+					log("Resposta recebida: '#{resposta}'.",0)
+					log("Resposta inválida.",0)
 				end
 			end
 	
-			log(file, "",1)
+			log("",1)
 			if okArray.size<numServers
 				okArray.each do |i|
-					log(file,"Enviando ABORT para #{hostnames[i]}:#{portas[i]}...",0)
+					log("Enviando ABORT para #{hostnames[i]}:#{portas[i]}...",0)
 					socket[i].send("ABORT",0)
-					log(file,"ABORT enviado para #{hostnames[i]}:#{portas[i]}....",0)
+					log("ABORT enviado para #{hostnames[i]}:#{portas[i]}....",0)
 				end
 			else
 				# enviar COMMIT com a alteração pra todo mundo.
-				log(file, "",1)
+				log("",1)
 				keyArray = []
 				numServers.times do |i|
-	 				log(file, "enviando um COMMIT para o #{socket[i]}",1)
+	 				log("enviando um COMMIT para o #{socket[i]}",1)
 					socket[i].send("COMMIT",0)
 					keyArray[i] = socket[i].recv(100)
-					log(file, "Palavra-chave recebida do host #{hostnames[i]}:#{portas[i].to_i} com sucesso! ",0)
+					log("Palavra-chave recebida do host #{hostnames[i]}:#{portas[i].to_i} com sucesso! ",0)
 				end
 				if keyArray.uniq.length > 1
-					log(file, "Palavras-chave recebidas são diferentes.",0)
+					log("Palavras-chave recebidas são diferentes.",0)
 					numServers.times do |i|
-						log(file, "Palavra-chave recebida de #{hostnames[i]}:#{portas[i].to_i}:",0)
-						log(file, keyArray[i],0)
-						log(file, "",0)
+						log("Palavra-chave recebida de #{hostnames[i]}:#{portas[i].to_i}:",0)
+						log(keyArray[i],0)
+						log("",0)
 					end
 				else
-					log(file, "Palavras-chave recebidas são iguais.",0)
-					log(file, "Palavra-chave: ",4)
-					log(file, keyArray.first,0)
-					log(file, "",0)
+					log("Palavras-chave recebidas são iguais.",0)
+					log("Palavra-chave: ",4)
+					log(keyArray.first,0)
+					log("",0)
 				end
 			end
 			
@@ -192,53 +192,53 @@ while opcao!=4
 			end
 	
 			numServers.times do |i| # envia GETFILE
-				log(file, "",1)
-	 			log(file, "Enviando requisição para #{hostnames[i]}:#{portas[i]}...",0)
+				log("",1)
+	 			log("Enviando requisição para #{hostnames[i]}:#{portas[i]}...",0)
 				socket[i].send("GETFILE",0)
-				log(file, "Requisição GETFILE enviada. Aguardando resposta...",0)
+				log("Requisição GETFILE enviada. Aguardando resposta...",0)
 				resposta = socket[i].recv(100)
 				if resposta == "OK"
 					okArray << i
-					log(file, "#{hostnames[i]}:#{portas[i]} respondeu OK.",0)
+					log("#{hostnames[i]}:#{portas[i]} respondeu OK.",0)
 				elsif resposta=="NOK" 
-					log(file, "#{hostnames[i]}:#{portas[i]} está ocupado.",0)
+					log("#{hostnames[i]}:#{portas[i]} está ocupado.",0)
 				else
-					log(file, "Resposta recebida: '#{resposta}'.",0)
-					log(file, "Resposta inválida.",0)
+					log("Resposta recebida: '#{resposta}'.",0)
+					log("Resposta inválida.",0)
 				end
 			end
 
-			log(file, "",1)
+			log("",1)
 			if okArray.size<numServers
 				okArray.each do |i|
-					log(file, "Enviando ABORT para #{hostnames[i]}:#{portas[i]}...",0)
+					log("Enviando ABORT para #{hostnames[i]}:#{portas[i]}...",0)
 					socket[i].send("ABORT",0)
-					log(file, "ABORT enviado para #{hostnames[i]}:#{portas[i]}....",0)
+					log("ABORT enviado para #{hostnames[i]}:#{portas[i]}....",0)
 				end
 			else
 				# enviar COMMIT com a alteração pra todo mundo.
 				arqArray = []
 				numServers.times do |i|
-					log(file, "",1)
-	 				log(file, "enviando um COMMIT para o #{socket[i]}",1)
+					log("",1)
+	 				log("enviando um COMMIT para o #{socket[i]}",1)
 					socket[i].send("COMMIT",0)
 					tamArq = socket[i].recv(100).to_i
 					socket[i].send("ACK",0)
 					arqArray[i] = socket[i].recv(tamArq)
-					log(file, "Arquivo de #{arqArray[i].size} bytes recebido do host #{hostnames[i]}:#{portas[i].to_i} com sucesso! ",0)
+					log("Arquivo de #{arqArray[i].size} bytes recebido do host #{hostnames[i]}:#{portas[i].to_i} com sucesso! ",0)
 				end
 				if arqArray.uniq.length > 1
 					puts "Arquivos recebidos são diferentes."
 					numServers.times do |i|
-						log(file, "Arquivo recebido de #{hostnames[i]}:#{portas[i].to_i}:",0)
-						log(file, "#{arqArray[i]}",0)
-						log(file, "",0)
+						log("Arquivo recebido de #{hostnames[i]}:#{portas[i].to_i}:",0)
+						log("#{arqArray[i]}",0)
+						log("",0)
 					end
 				else
-					log(file, "Arquivos recebidos são iguais.",0)
-					log(file, "Arquivo recebido:",0)
-					log(file, "#{arqArray.first}",0)
-					log(file, "",0)
+					log("Arquivos recebidos são iguais.",0)
+					log("Arquivo recebido:",0)
+					log("#{arqArray.first}",0)
+					log("",0)
 				end
 			end
 			
@@ -247,12 +247,12 @@ while opcao!=4
 			end
 		when 4			
 		else # EXIT
-			log(file, "Opcao inválida",0)
+			log("Opcao inválida",0)
 	end
 
-	log(file, "",1)
+	log("",1)
 end
-log(file, "Saindo do Cliente.",1)
-file.close
+log("Saindo do Cliente.",1)
+$file.close
 
 
--- servidor.rb
+++ servidor.rb
@@ -16,27 +16,27 @@ filePath = "teste"
 
 #fazer funcao\/
 key = File.new("keyword.txt", "r+")
-keyword = key.read.chomp
+$keyword = key.read.chomp
 key.close
 
 filename = "servidor#{port}.txt"
-file = File.new(filename, "w+")
+$file = File.new(filename, "w+")
 #======================#
 #        metodos       #
 #======================#
 
 
-def log(file, msg, val)
+def log(msg, val)
 	case val
 		when 0
-			file.puts msg
+			$file.puts msg
 			puts msg
 		when 1
-			file.puts msg
+			$file.puts msg
 		when 2
 			puts msg
 		when 3
-			file.print msg
+			$file.print msg
 			print msg
 		when 4
 			print msg
@@ -49,24 +49,24 @@ def threadId
 	return "Thread #{Thread.current.object_id}"
 end
 
-def shut_down (file, keyword)
-	log(file, "\n",1)
-	log(file, "Saindo do Servidor.",1)
+def shut_down
+	log("\n",1)
+	log("Saindo do Servidor.",1)
 	#salvando chave no arquivo
-	log(file, "Salvando palavra chave #{keyword} no arquivo keyword.txt",1)
+	log("Salvando palavra chave #{$keyword} no arquivo keyword.txt",1)
 	key = File.new("keyword.txt", "w")
-	key.puts keyword
-	file.close
+	key.puts $keyword
 	key.close
+	$file.close
 end 
 
 Signal.trap("INT") { 
-  shut_down(file, keyword)
+  shut_down
   exit 
 } 
 
 Signal.trap("TERM") { 
-  shut_down(file, keyword)
+  shut_down
   exit 
 }
 
@@ -76,11 +76,11 @@ Signal.trap("TERM") {
 #======================#
 
 
-log(file, " -----------------------------------------------------------------------",1)
-log(file, "| Prof. Elias P. Duarte Jr.  -  Disciplina Redes 2                      |",1)
-log(file, "| Trabalho que implementa a Consistência de Dados com 2PC Simplificado  |",1)
-log(file, " -----------------------------------------------------------------------",1)
-log(file, "Servidor: #{port}",1)
+log(" -----------------------------------------------------------------------",1)
+log("| Prof. Elias P. Duarte Jr.  -  Disciplina Redes 2                      |",1)
+log("| Trabalho que implementa a Consistência de Dados com 2PC Simplificado  |",1)
+log(" -----------------------------------------------------------------------",1)
+log("Servidor: #{port}",1)
 
 mutex = Mutex.new
 contClient = 0
@@ -89,128 +89,128 @@ loop {
 #servidor com mais de um cliente
 	Thread.start(server.accept) do |client|
 		clientId = contClient
-		log(file, "", 0)
-		log(file, "#{threadId}: Conexão estabelecida com o cliente #{clientId}",0)
-		log(file, "#{threadId}: Aguardando requisicao...",0)
+		log("", 0)
+		log("#{threadId}: Conexão estabelecida com o cliente #{clientId}",0)
+		log("#{threadId}: Aguardando requisicao...",0)
 		requisicao = client.recv(100)
-		log(file, "#{threadId}: Requisição recebida: '#{requisicao}'.",0)
+		log("#{threadId}: Requisição recebida: '#{requisicao}'.",0)
 		case requisicao
 		when "SETKEY"
 			if !mutex.try_lock
-				log(file, "#{threadId}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}...",0)
+				log("#{threadId}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}...",0)
 				client.send("NOK",0)
-				log(file, "#{threadId}: NOK enviado para cliente #{clientId}.",0)
+				log("#{threadId}: NOK enviado para cliente #{clientId}.",0)
 			else
-				log(file, "#{threadId}: Enviando OK para cliente #{clientId}...",0)
+				log("#{threadId}: Enviando OK para cliente #{clientId}...",0)
 				client.send("OK",0)
-				log(file, "#{threadId}: OK enviado para cliente #{clientId}.",0)
-				log(file, "#{threadId}: Aguardando nova requisição do cliente #{clientId}...",0)
+				log("#{threadId}: OK enviado para cliente #{clientId}.",0)
+				log("#{threadId}: Aguardando nova requisição do cliente #{clientId}...",0)
 				requisicao = client.recv(100)
 				if requisicao == "COMMIT"
 					client.send("ACK",0)
-					log(file, "#{threadId}: Requisição recebida: '#{requisicao}'.",0)
-					oldKey = keyword
-					keyword = client.recv(100)
-					log(file, "#{threadId}: Criptografando arquivo...",0)
+					log("#{threadId}: Requisição recebida: '#{requisicao}'.",0)
+					oldKey = $keyword
+					$keyword = client.recv(100)
+					log("#{threadId}: Criptografando arquivo...",0)
 					File.open(filePath, "r+") do |f|
-						log(file, "Arquivo cifrado com a antiga palavra chave #{oldKey}:",1)
-						log(file, "#{f.read}",1)
+						log("Arquivo cifrado com a antiga palavra chave #{oldKey}:",1)
+						log("#{f.read}",1)
 						f.rewind
-						log(file,"",1)
-						log(file, "Descifrado com a chave #{oldKey}: ",1)
+						log("",1)
+						log("Descifrado com a chave #{oldKey}: ",1)
 						textoClaro = VigenereCipher.decrypt(f.read, oldKey)
-						log(file, "Arquivo descifrado:",1)
-						log(file, "#{textoClaro}",1)
-						log(file, "",1)
+						log("Arquivo descifrado:",1)
+						log("#{textoClaro}",1)
+						log("",1)
 						f.rewind
 						f.truncate(0)
-						log(file, "Novo arquivo cifrado com a chave #{keyword}:",1)
-						f.write VigenereCipher.encrypt(textoClaro,keyword)
+						log("Novo arquivo cifrado com a chave #{$keyword}:",1)
+						f.write VigenereCipher.encrypt(textoClaro,$keyword)
 						f.rewind 
-						log(file, "#{f.read}",1)
-						log(file, "",0)
+						log("#{f.read}",1)
+						log("",0)
 						textoClaro = nil
 						
 					end
 					
-					log(file, "#{threadId}: Chave alterada de '#{oldKey}' para '#{keyword}'.",0)
-					# log(file, "#{threadId}: sleeping 10sec.",0)
+					log("#{threadId}: Chave alterada de '#{oldKey}' para '#{$keyword}'.",0)
+					# log("#{threadId}: sleeping 10sec.",0)
 					# sleep 10
-					# log(file, "#{threadId}: wake!",0)
+					# log("#{threadId}: wake!",0)
 					# end	
 				elsif requisicao == "ABORT"
-					log(file, "#{threadId}: Requisição recebida '#{requisicao}'.",0)
-					log(file, "#{threadId}: Operacao Cancelada",0)
+					log("#{threadId}: Requisição recebida '#{requisicao}'.",0)
+					log("#{threadId}: Operacao Cancelada",0)
 				else 
-					log(file, "#{threadId}: Requisição recebida: '#{requisicao}'.",0)
-					log(file, "#{threadId}: Resposta inválida",0)
-					log(file, "#{threadId}: Resposta esperada: COMMIT ou ABORT.",0)
+					log("#{threadId}: Requisição recebida: '#{requisicao}'.",0)
+					log("#{threadId}: Resposta inválida",0)
+					log("#{threadId}: Resposta esperada: COMMIT ou ABORT.",0)
 				end
 				mutex.unlock
 			end
 		when "GETFILE"
 			if !mutex.try_lock
-				log(file, "#{threadId}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}...",0)
+				log("#{threadId}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}...",0)
 				client.send("NOK",0)
-				log(file, "#{threadId}: NOK enviado para cliente #{clientId}.",0)
+				log("#{threadId}: NOK enviado para cliente #{clientId}.",0)
 			else
-				log(file, "#{threadId}: Enviando OK para cliente #{clientId}...",0)
+				log("#{threadId}: Enviando OK para cliente #{clientId}...",0)
 				client.send("OK",0)
-				log(file, "#{threadId}: OK enviado para cliente #{clientId}.",0)
-				log(file, "#{threadId}: Aguardando nova requisição do cliente #{clientId}...",0)
+				log("#{threadId}: OK enviado para cliente #{clientId}.",0)
+				log("#{threadId}: Aguardando nova requisição do cliente #{clientId}...",0)
 				requisicao = client.recv(100)
 				if requisicao == "COMMIT"
-					log(file, "#{threadId}: Requisição recebida: '#{requisicao}'.",0)
+					log("#{threadId}: Requisição recebida: '#{requisicao}'.",0)
 					File.open(filePath,"r") do |f|
-						log( file, "#{threadId}: Arquivo de #{f.size} bytes aberto para leitura.",0)
+						log("#{threadId}: Arquivo de #{f.size} bytes aberto para leitura.",0)
 						client.send(f.size.to_s,0)
 						confirmacao = client.recv(100)
 						if confirmacao=="ACK"
 							client.send(f.read, 0)
-							log(file, "#{threadId}: Arquivo enviado para o cliente #{clientId} com sucesso!",0)
+							log("#{threadId}: Arquivo enviado para o cliente #{clientId} com sucesso!",0)
 						else
-							log(file, "#{threadId}: Falha ao receber confirmação do cliente #{clientId}.",0)
-							log(file, "#{threadId}: Mensagem do servidor: '#{confirmacao}'.",0)
+							log("#{threadId}: Falha ao receber confirmação do cliente #{clientId}.",0)
+							log("#{threadId}: Mensagem do servidor: '#{confirmacao}'.",0)
 						end
 					end
 				elsif requisicao == "ABORT"
-					log(file, "#{threadId}: Requisição recebida '#{requisicao}'.",0)
-					log(file, "#{threadId}: Operacao Cancelada",0)
+					log("#{threadId}: Requisição recebida '#{requisicao}'.",0)
+					log("#{threadId}: Operacao Cancelada",0)
 				else 
-					log(file, "#{threadId}: Requisição recebida: '#{requisicao}'.",0)
-					log(file, "#{threadId}: Resposta inválida",0)
-					log(file, "#{threadId}: Resposta esperada: COMMIT ou ABORT.",0)
+					log("#{threadId}: Requisição recebida: '#{requisicao}'.",0)
+					log("#{threadId}: Resposta inválida",0)
+					log("#{threadId}: Resposta esperada: COMMIT ou ABORT.",0)
 				end
 				mutex.unlock
 			end
 		when "GETKEY"
 			if !mutex.try_lock
-				log(file, "#{threadId}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}...",0)
+				log("#{threadId}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}...",0)
 				client.send("NOK",0)
-				log(file, "#{threadId}: NOK enviado para cliente #{clientId}.",0)
+				log("#{threadId}: NOK enviado para cliente #{clientId}.",0)
 			else
-				log(file, "#{threadId}: Enviando OK para cliente #{clientId}...",0)
+				log("#{threadId}: Enviando OK para cliente #{clientId}...",0)
 				client.send("OK",0)
-				log(file, "#{threadId}: OK enviado para cliente #{clientId}.",0)
-				log(file, "#{threadId}: Aguardando nova requisição do cliente #{clientId}...",0)
+				log("#{threadId}: OK enviado para cliente #{clientId}.",0)
+				log("#{threadId}: Aguardando nova requisição do cliente #{clientId}...",0)
 				requisicao = client.recv(100)
-				log(file, "#{threadId}: Requisição recebida: '#{requisicao}'.",0)
+				log("#{threadId}: Requisição recebida: '#{requisicao}'.",0)
 				if requisicao == "COMMIT"
-					client.send(keyword,0)
-					log(file, "#{threadId}: Palavra-chave '#{keyword}' enviada para o cliente #{clientId} com sucesso!",0)
+					client.send($keyword,0)
+					log("#{threadId}: Palavra-chave '#{$keyword}' enviada para o cliente #{clientId} com sucesso!",0)
 				elsif requisicao == "ABORT"
-					log(file, "#{threadId}: Operacao Cancelada",0)
+					log("#{threadId}: Operacao Cancelada",0)
 				else
-					log(file, "#{threadId}: Resposta inválida",0)
-					log(file, "#{threadId}: Resposta esperada: COMMIT ou ABORT.",0)
+					log("#{threadId}: Resposta inválida",0)
+					log("#{threadId}: Resposta esperada: COMMIT ou ABORT.",0)
 				end
 				mutex.unlock
 			end
  		else
- 			log(file, "#{threadId}: Resposta inválida.",0)
-			log(file, "#{threadId}: (Respostas esperadas: SETKEY, GETKEY, GETFILE.)",0)
+ 			log("#{threadId}: Resposta inválida.",0)
+			log("#{threadId}: (Respostas esperadas: SETKEY, GETKEY, GETFILE.)",0)
 		end
- 		log(file, "#{threadId}: Fechando Conexão com o cliente #{clientId}.",0 )
+ 		log("#{threadId}: Fechando Conexão com o cliente #{clientId}.",0 )
 		
 		client.close
 	end
