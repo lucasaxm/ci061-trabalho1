10b32117a69c7d1542e1f5b6562497004f05d0cb
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2015-05-31 23:34:20 -0300

    criado uma parte do log, falta a parte dos dados
    Signed-off-by: Evelim Carla Ribeiro <ecr10@inf.ufpr.br>

--- cliente.rb
+++ cliente.rb
@@ -7,30 +7,47 @@ numServers=3
 hostnames = []
 portas = []
 socket = []
+filename = "cliente.txt"
+file = File.new(filename, "w+")
+file.puts " -----------------------------------------------------------------------"
+file.puts "| Prof. Elias P. Duarte Jr.  -  Disciplina Redes 2                      |"
+file.puts "| Trabalho que implementa a Consistência de Dados com 2PC Simplificado  |"
+file.puts " -----------------------------------------------------------------------"
+file.puts "Cliente \n"
 numServers.times do |i|
 	puts "Digite o nome do servidor #{i}: "
 	hostnames[i]=gets.chomp	# array com nome dos servidores
 	puts "Digite a porta do servidor #{i}: "
 	portas[i]=gets.chomp	# array com porta dos servidores
+	file.puts "Inserindo o servidor #{hostnames[i]} com #{porta[i]} para poder fazer conexão."
 end
 opcao = 0
 system "clear"
 while opcao!=2
-	print "Servidores: "
+	file.puts "Este cliente esta conectado aos seguintes servidores: "
+	print "Este cliente esta conectado aos seguintes servidores: "
 	numServers.times do |i|
 		if i!=2
+			file.print "#{hostnames[i]}:#{portas[i]}, "
 			print "#{hostnames[i]}:#{portas[i]}, "
 		else
+			file.puts "#{hostnames[i]}:#{portas[i]}."
 			puts "#{hostnames[i]}:#{portas[i]}."
 		end
 	end
+	file.puts"
+	Escolha uma opção
+	1 - Editar arquivo no servidor.
+	2 - Sair.
+	? "
+
 	print "
 	Escolha uma opção
 	1 - Editar arquivo no servidor.
 	2 - Sair.
 	? "
 	opcao = gets.chomp.to_i
-
+	file.puts "opcao digitada #{opcao}"
 	if opcao==1
 		okArray=[]
 
@@ -39,47 +56,66 @@ while opcao!=2
 		end
 
 	 	numServers.times do |i|
+	 		file.puts "\n"
+	 		file.puts "Enviando requisição para #{hostnames[i]}:#{portas[i]}..."
 	 		puts "Enviando requisição para #{hostnames[i]}:#{portas[i]}..."
 	 		socket[i].send("EDIT",0)
+	 		file.puts "Requisição EDIT enviada. Aguardando resposta..."
 	 		puts "Requisição EDIT enviada. Aguardando resposta..."
 	 		resposta = socket[i].recv(100)
 	 		if resposta == "OK"
 	 			okArray << i
+	 			file.puts "#{hostnames[i]}:#{portas[i]} respondeu OK."
 	 			puts "#{hostnames[i]}:#{portas[i]} respondeu OK."
 	 		elsif resposta=="NOK" 
+	 			file.puts "#{hostnames[i]}:#{portas[i]} está ocupado."
 	 			puts "#{hostnames[i]}:#{portas[i]} está ocupado."
 	 		else
+	 			file.puts "Resposta recebida: '#{resposta}'."
+	 			file.puts "Resposta inválida."
 	 			puts "Resposta recebida: '#{resposta}'."
 	 			puts "Resposta inválida."
 	 		end
 	 	end
 
+	 	file.puts "\n"
  		if okArray.size<numServers
  			okArray.each do |i|
+ 				file.puts "Enviando ABORT para #{hostnames[i]}:#{portas[i]}..."
  				puts "Enviando ABORT para #{hostnames[i]}:#{portas[i]}..."
  				socket[i].send("ABORT",0)
+ 				file.puts "ABORT enviado para #{hostnames[i]}:#{portas[i]}...."
  				puts "ABORT enviado para #{hostnames[i]}:#{portas[i]}...."
  			end
  		else
 	 		# enviar COMMIT com a alteração pra todo mundo.
 	 		puts "Digite o novo valor do dado:"
+	 		file.puts "Digite o novo valor do dado:"
 	 		dado = gets.chomp
+	 		file.puts "O dado digitado é: #{dado}"
 	 		numServers.times do |i|
+	 			file.puts "\n"
+	 			file.puts "enviando um COMMIT para o #{socket[i]}"
 	 			socket[i].send("COMMIT",0)
 	 			confirmacao = socket[i].recv(100)
+	 			file.puts "recebendo a confirmação #{confirmacao} do #{socket[i]}"
 	 			if confirmacao=="ACK"
 	 				socket[i].send(dado, 0)
+	 				file.puts "String '#{dado}' enviada para o host #{hostnames[i]}:#{portas[i].to_i} com sucesso!"
 	 				puts "String '#{dado}' enviada para o host #{hostnames[i]}:#{portas[i].to_i} com sucesso!"
 	 			else
+	 				file.puts "Falha ao receber confirmação do servidor."
+	 				file.puts "Mensagem do servidor: '#{confirmacao}'."
 	 				puts "Falha ao receber confirmação do servidor."
 	 				puts "Mensagem do servidor: '#{confirmacao}'."
 	 			end
 	 			
 	 		end
 	 	end
-	 	
 		socket.each do |s|	# fecha sockets
 			s.close
 		end
 	end
+ 	file.puts "\n"
+ 	file.puts "Saindo do Cliente."
 end
--- servidor.rb
+++ servidor.rb
@@ -9,40 +9,66 @@ dado = "My precious Taz and Toph"
 mutex = Mutex.new
 contClient = 0
 server = TCPServer.open(port)
+filename = "servidor#{port}.txt"
+file = File.new(filename, "w+")
+file.puts " -----------------------------------------------------------------------"
+file.puts "| Prof. Elias P. Duarte Jr.  -  Disciplina Redes 2                      |"
+file.puts "| Trabalho que implementa a Consistência de Dados com 2PC Simplificado  |"
+file.puts " -----------------------------------------------------------------------"
+file.puts "Servidor: #{port}"
 loop {
 #servidor com mais de um cliente
 	Thread.start(server.accept) do |client|
 		clientId = contClient
+		file.puts "\n"
+		ap "\n"
+		file.puts "Thread #{Thread.current.object_id}: Conexão estabelecida com o cliente #{clientId}"
+		file.puts "Thread #{Thread.current.object_id}: Aguardando requisicao..."
 		ap "Thread #{Thread.current.object_id}: Conexão estabelecida com o cliente #{clientId}"
 		ap "Thread #{Thread.current.object_id}: Aguardando requisicao..."
 		requisicao = client.recv(100)
+		file.puts "Thread #{Thread.current.object_id}: Requisição recebida: '#{requisicao}'."
 		ap "Thread #{Thread.current.object_id}: Requisição recebida: '#{requisicao}'."
 		if (requisicao == "EDIT")
 			if !mutex.try_lock
+				file.puts "Thread #{Thread.current.object_id}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}..."
 				ap "Thread #{Thread.current.object_id}: Outro cliente está alterando o dado. Enviando NOK para cliente #{clientId}..."
 				client.send("NOK",0)
+				file.puts "Thread #{Thread.current.object_id}: NOK enviado para cliente #{clientId}."
 				ap "Thread #{Thread.current.object_id}: NOK enviado para cliente #{clientId}."
 			else
+				file.puts "Thread #{Thread.current.object_id}: Enviando OK para cliente #{clientId}..."
 				ap "Thread #{Thread.current.object_id}: Enviando OK para cliente #{clientId}..."
 				client.send("OK",0)
+				file.puts "Thread #{Thread.current.object_id}: OK enviado para cliente #{clientId}."
+				file.puts "Thread #{Thread.current.object_id}: Aguardando nova requisição do cliente #{clientId}..."
 				ap "Thread #{Thread.current.object_id}: OK enviado para cliente #{clientId}."
 				ap "Thread #{Thread.current.object_id}: Aguardando nova requisição do cliente #{clientId}..."
 				requisicao = client.recv(100)
 				if requisicao == "COMMIT"
 					# mutex.synchronize do
 					client.send("ACK",0)
+					file.puts "Thread #{Thread.current.object_id}: Requisição recebida: '#{requisicao}'."
 					ap "Thread #{Thread.current.object_id}: Requisição recebida: '#{requisicao}'."
 					dadoOld = dado
 					dado = client.recv(100)
+					# file.puts "Thread #{Thread.current.object_id}: sleeping 10sec."
 					# puts "Thread #{Thread.current.object_id}: sleeping 10sec."
 					# sleep 10
+					# file.puts "Thread #{Thread.current.object_id}: wake!"
 					# puts "Thread #{Thread.current.object_id}: wake!"
+					file.puts "Thread #{Thread.current.object_id}: Dado alterado de '#{dadoOld}' para '#{dado}'."
 					ap "Thread #{Thread.current.object_id}: Dado alterado de '#{dadoOld}' para '#{dado}'."
 					# end	
 				elsif requisicao == "ABORT"
+					file.puts "Thread #{Thread.current.object_id}: Requisição recebida '#{requisicao}'."
+					file.puts "Thread #{Thread.current.object_id}: Operacao Cancelada"
 					ap "Thread #{Thread.current.object_id}: Requisição recebida '#{requisicao}'."
 					ap "Thread #{Thread.current.object_id}: Operacao Cancelada"
-				else 
+				else
+					file.puts "Thread #{Thread.current.object_id}: Requisição recebida: '#{requisicao}'."
+					file.puts "Thread #{Thread.current.object_id}: Resposta inválida"
+					file.puts "Thread #{Thread.current.object_id}: Resposta esperada: COMMIT ou ABORT." 
 					ap "Thread #{Thread.current.object_id}: Requisição recebida: '#{requisicao}'."
 					ap "Thread #{Thread.current.object_id}: Resposta inválida"
 					ap "Thread #{Thread.current.object_id}: Resposta esperada: COMMIT ou ABORT."
@@ -50,12 +76,16 @@ loop {
 				mutex.unlock
 			end										
  		else
+ 			file.puts "Thread #{Thread.current.object_id}: Resposta inválida."
+			file.puts "Thread #{Thread.current.object_id}: (Resposta esperada: EDIT.)"
  			ap "Thread #{Thread.current.object_id}: Resposta inválida."
 			ap "Thread #{Thread.current.object_id}: (Resposta esperada: EDIT.)"
- 		end 
- 		ap "Thread #{Thread.current.object_id}: Fechando Conexão com o cliente #{clientId}." 
-		
+ 		end
+
+ 		file.puts "Thread #{Thread.current.object_id}: Fechando Conexão com o cliente #{clientId}." 
+ 		ap "Thread #{Thread.current.object_id}: Fechando Conexão com o cliente #{clientId}." 		
 		client.close
 	end
 		contClient+=1
-}  
+}
+file.close
